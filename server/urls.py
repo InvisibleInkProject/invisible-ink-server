25e9ad89123d19f1e8129fd95a1cc3f4bc8f2d07
Xennis <code@xennis.org>
2014-02-17 20:19:34 +0100

    remove comments

--- server/urls.py
+++ server/urls.py
@@ -1,21 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from inks.resources import MessageResource
-#from ink.views import GetByLocationView
 from tastypie.api import Api
 
 import inks.models
 
 admin.autodiscover()
-#admin.site.register(ink.models.Message)
-#admin.site.register(ink.models.User)
 
 api = Api(api_name='v1')    
 api.register(MessageResource())
 
 urlpatterns = patterns('',
     # Examples:
-#    url(r'^twitter/$', GetByLocationView.as_view(), name='home'),
     url(r'^api/', include(api.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
